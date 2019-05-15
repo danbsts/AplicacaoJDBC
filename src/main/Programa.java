284019f9153288f7256b5e62d0be6add67207e17
danbsts <dmrb2@cin.ufpe.br>
2019-05-15 16:49:19 -0300

    consulta sql

--- src/main/Programa.java
+++ src/main/Programa.java
@@ -16,8 +16,8 @@ public class Programa {
 			System.out.println("3 - Inserir Multimida");
 			System.out.println("4 - Visualizar Multimida");
 			System.out.println("0 - Para fechar o programa");
-			int opcao = Integer.parseInt(in.nextLine());
-			if (opcao == 1) { // inserir pessoa
+			comando = Integer.parseInt(in.nextLine());
+			if (comando == 1) { // inserir pessoa
 				// (CPF_p, Nome, Data_de_nascimento, Sexo)
 				String cpf, nome, dataNasc, genero;
 				System.out.println("Por favor digite os dados da pessoa a ser cadastrada.");
@@ -30,18 +30,32 @@ public class Programa {
 				System.out.print("Por favor digite o sexo: ");
 				genero = in.nextLine();
 				jdbc.adicionarPessoa(cpf, nome, dataNasc, genero);
-			} else if(opcao == 2) { // consulta pessoa
+			} else if(comando == 2) { // consulta pessoa
 				int escolhido = 1;
-				while(escolhido < 1 || escolhido > 2) {
+				while(escolhido < 0 || escolhido > 2) {
 					System.out.println("Digite a opção de sua escolha:");
-					System.out.println("1 - Para selecionar toda tabela.");
-					System.out.println("2 - Para selecionar atributos especificos.");
+					System.out.println("1 - Para selecionar todos da tabela.");
+					System.out.println("2 - Para selecionar atributos que satisfazem uma condicao especifica (WHERE).");
 					System.out.println("0 - Para selecionar cancelar.");
 					escolhido = Integer.parseInt(in.nextLine());
+					String colunas;
+					if(escolhido == 1 || escolhido == 2) {
+						System.out.println("Digite as colunas, na mesma linha, que você deseja selecionar:");
+						System.out.println("Para todas as colunas, digite \\*.");
+						colunas = in.nextLine();
+						if(escolhido == 1) {
+							jdbc.selectInstrucao(colunas);
+						} else {
+							System.out.println("Por favor digite as condicoes como em uma consulta SQL");
+							String condicoes = in.nextLine();
+							jdbc.selectInstrucao(colunas, condicoes);
+						}
+						
+					}
 				}
-			} else if(opcao == 3) { // inserir multimidia
+			} else if(comando == 3) { // inserir multimidia
 				
-			} else if(opcao == 4) { // visualizar multimidia
+			} else if(comando == 4) { // visualizar multimidia
 				
 			} else { // fechar
 				comando = 0;
