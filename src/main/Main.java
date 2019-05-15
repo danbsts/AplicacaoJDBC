aa077ea4093eb1844e8186a2a89e6139dd1ee07f
rodrigoap2 <rap2@cin.ufpe.br>
2019-05-15 10:07:57 -0300

    Testando o arquivo jdbc do slide

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,87 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-	public static void main(String args[]) {
-		//bom diaaa
-	}
+
+    public static String url = "jdbc:oracle:thin:@oracle12c.cin.ufpe.br:1521:instance01";
+    public static String user = "g191if685cc_eq05";
+    public static String senha = "myjxmwoh";
+
+    public static Connection conexao;
+    public static Statement stmt;
+
+    public static ResultSet resultado;
+
+    public static void createConnection(){
+
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conexao = DriverManager.getConnection(url, user, senha);
+            stmt = conexao.createStatement();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void closeConnection(){
+        try {
+            conexao.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectBasico(String coluna, String tabela){
+
+        String consultaSql = "select " + coluna + " from " + tabela;
+
+        try {
+            resultado = stmt.executeQuery(consultaSql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void ImprimirResultado(){
+        while(true){
+            try {
+                if (resultado.next()){
+
+                    System.out.println(resultado.getString(1));
+                }else{
+                    break;
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String args[]){
+
+        //Fazer um select basico:
+
+        //Abrir a conexão:
+        createConnection();
+
+        //Fazer um select:
+        selectBasico("*","JOGADOR");
+
+        //Imprimir resultado:
+        ImprimirResultado();
+
+        //Fechar a conexão;
+        closeConnection();
+    }
+
 }
